c30e8c15b1532ad9731c19ecce2280d86d124287
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-01-11 20:03:25 -0700

    Removed unnecessary code

--- src/auth.js
+++ src/auth.js
@@ -1,30 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var querystring = require("querystring");
 var dotenv = require("dotenv");
 dotenv.config();
 var WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
-// import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api"; // Supports ESM
 var WooCommerce = new WooCommerceRestApi({
     url: "https://vivantaire.com/",
     consumerKey: process.env.CONSUMER_KEY,
     consumerSecret: process.env.CONSUMER_SECRET,
     version: "wc/v3",
 });
-var store_url = "https://vivantaire.com/";
-var endpoint = "/wc-auth/v1/authorize";
-var params = {
-    app_name: "Jewelry Products",
-    scope: "read_write",
-    user_id: 123,
-    return_url: "http://app.com/return-page",
-    callback_url: "https://app.com/callback-endpoint",
-};
-var queryString = querystring.stringify(params).replace(/%20/g, "+");
-console.log(store_url + endpoint + "?" + queryString);
 WooCommerce.get("products")
     .then(function (response) {
     console.log("Products:", response.data);
+    response.data.forEach(function (product) {
+        var name = product.name;
+        var sku = product.sku;
+        var stock = product.stock_quantity;
+        var price = product.price;
+        console.log(name);
+        console.log(sku);
+        console.log(stock);
+        console.log(price);
+    });
 })
     .catch(function (error) {
     console.error("Error retrieving products:", error);
--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,8 @@
-import querystring = require("querystring");
-import dotenv = require("dotenv")
-dotenv.config()
+import dotenv = require("dotenv");
+
+dotenv.config();
 
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
-// import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api"; // Supports ESM
 
 const WooCommerce = new WooCommerceRestApi({
     url: "https://vivantaire.com/",
@@ -12,30 +11,21 @@ const WooCommerce = new WooCommerceRestApi({
     version: "wc/v3",
 });
 
-type Params = {
-    app_name: string
-    scope: string
-    user_id: number
-    return_url: string
-    callback_url: string
-}
-
-const store_url: string = "https://vivantaire.com/";
-const endpoint: string = "/wc-auth/v1/authorize";
-const params: Params = {
-    app_name: "Jewelry Products",
-    scope: "read_write",
-    user_id: 123,
-    return_url: "http://app.com/return-page",
-    callback_url: "https://app.com/callback-endpoint",
-};
-const queryString: string = querystring.stringify(params).replace(/%20/g, "+");
-
-console.log(store_url + endpoint + "?" + queryString);
-
 WooCommerce.get("products")
     .then((response) => {
         console.log("Products:", response.data);
+
+        response.data.forEach((product) => {
+            const name = product.name;
+            const sku = product.sku;
+            const stock = product.stock_quantity;
+            const price = product.price;
+
+            console.log(name);
+            console.log(sku);
+            console.log(stock);
+            console.log(price);
+        })
     })
     .catch((error) => {
         console.error("Error retrieving products:", error);
